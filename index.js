7b347c883a30ac9795a91dc3d08f776eb352bb3c
Donovan <divdonis@gmail.com>
2021-04-30 20:16:47 -0700

    commit tree 9a15fe12e027191e0d396b5b5f605feb2f27461e

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = parseInt(num2) * parseInt(num1);
+
+let random = parseInt(Math.random());
+++random;
+
+let num3 = 29;
+let num4 = 5;
+let mod = parseInt(num3) % parseInt(num4);
+
+let max = parseInt(Math.max(1,2,4,20));
